73810fd1f498d879326013cab4cdaf9e6a8110ae
Merve <gulineyel@gmail.com>
2020-10-25 18:21:52 +0300

    first lecture start

---
+++ src/pages/RestaurantDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const RestaurantDetail = (props) => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>
+                    RestauranL Detail
+                </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export { RestaurantDetail };
---
+++ src/pages/RestaurantList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const RestaurantList = (props) => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>
+                    RestauranList
+                </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export { RestaurantList };
