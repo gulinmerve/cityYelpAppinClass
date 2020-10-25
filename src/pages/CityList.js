b66905c16884082733a553163892f45e845add90
Merve <gulineyel@gmail.com>
2020-10-25 19:48:15 +0300

    search/filtered

--- src/pages/CityList.js
+++ src/pages/CityList.js
@@ -5,12 +5,14 @@ import { SafeAreaView, View, Text, FlatList } from 'react-native';
 import { CityItem, SearchBar } from '../components'
 
 const CityList = (props) => {
+    const [originalList,setOriginalList] = useState([]);
     const [cityList, setCityList] = useState([]);
 
     // ASYNC-AWAIT 
     const fetchCityData = async () => {
         const { data } = await axios.get("https://opentable.herokuapp.com/api/cities");
         setCityList(data.cities);
+        setOriginalList(data.cities);
     }
 
     useEffect(() => {
@@ -18,14 +20,25 @@ const CityList = (props) => {
     }, [])
 
     const renderCities = ({ item }) => <CityItem cityName={item} />
+
     const renderSeperator = () => <View style={{borderWidth:1, borderColor:'#e0e0e0'}}></View>
+
+    function searchCity(search) {
+        const filteredCities = originalList.filter(city => {
+            const text = search.toUpperCase();
+            const cityName = city.toUpperCase();
+
+            return cityName.indexOf(text) > -1;
+        })
+        setCityList(filteredCities);
+    }
 
     return (
         <SafeAreaView>
             <View>
                 <SearchBar 
-                placeholder="Bir şehir arayın..."
-                onSearch={(value)=>console.log(value)}
+                placeholder="Search a city..."
+                onSearch={(value)=>searchCity(value)}
                 />
                 <FlatList
                     keyExtractor={(_, index) => index.toString()}
